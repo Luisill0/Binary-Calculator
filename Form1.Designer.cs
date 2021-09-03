f0c9373c2fee966ff1367c6390471c62f3a6c47e
Luis Naranjo <a288074@alumnos.uaslp.mx>
2021-09-03 15:40:05 -0500

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace BinaryCalculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button0 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonAND = new System.Windows.Forms.Button();
+            this.buttonOR = new System.Windows.Forms.Button();
+            this.buttonXOR = new System.Windows.Forms.Button();
+            this.buttonNOT = new System.Windows.Forms.Button();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textOperation = new System.Windows.Forms.TextBox();
+            this.textOperator1 = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(171, 365);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(141, 60);
+            this.button0.TabIndex = 0;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(24, 37);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(458, 193);
+            this.textBox.TabIndex = 3;
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonAND
+            // 
+            this.buttonAND.Location = new System.Drawing.Point(171, 272);
+            this.buttonAND.Name = "buttonAND";
+            this.buttonAND.Size = new System.Drawing.Size(141, 60);
+            this.buttonAND.TabIndex = 11;
+            this.buttonAND.Text = "AND";
+            this.buttonAND.UseVisualStyleBackColor = true;
+            this.buttonAND.Click += new System.EventHandler(this.buttonAND_Click);
+            // 
+            // buttonOR
+            // 
+            this.buttonOR.Location = new System.Drawing.Point(327, 272);
+            this.buttonOR.Name = "buttonOR";
+            this.buttonOR.Size = new System.Drawing.Size(141, 60);
+            this.buttonOR.TabIndex = 11;
+            this.buttonOR.Text = "OR";
+            this.buttonOR.UseVisualStyleBackColor = true;
+            this.buttonOR.Click += new System.EventHandler(this.buttonOR_Click);
+            // 
+            // buttonXOR
+            // 
+            this.buttonXOR.Location = new System.Drawing.Point(327, 365);
+            this.buttonXOR.Name = "buttonXOR";
+            this.buttonXOR.Size = new System.Drawing.Size(141, 60);
+            this.buttonXOR.TabIndex = 11;
+            this.buttonXOR.Text = "XOR";
+            this.buttonXOR.UseVisualStyleBackColor = true;
+            this.buttonXOR.Click += new System.EventHandler(this.buttonXOR_Click);
+            // 
+            // buttonNOT
+            // 
+            this.buttonNOT.Location = new System.Drawing.Point(327, 447);
+            this.buttonNOT.Name = "buttonNOT";
+            this.buttonNOT.Size = new System.Drawing.Size(141, 60);
+            this.buttonNOT.TabIndex = 11;
+            this.buttonNOT.Text = "NOT";
+            this.buttonNOT.UseVisualStyleBackColor = true;
+            this.buttonNOT.Click += new System.EventHandler(this.buttonNOT_Click);
+            // 
+            // buttonEquals
+            // 
+            this.buttonEquals.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEquals.Location = new System.Drawing.Point(171, 444);
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.Size = new System.Drawing.Size(141, 60);
+            this.buttonEquals.TabIndex = 11;
+            this.buttonEquals.Text = "=";
+            this.buttonEquals.UseVisualStyleBackColor = true;
+            this.buttonEquals.Click += new System.EventHandler(this.buttonEquals_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(24, 444);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(141, 60);
+            this.buttonClear.TabIndex = 11;
+            this.buttonClear.Text = "C";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(24, 272);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(141, 60);
+            this.buttonDelete.TabIndex = 11;
+            this.buttonDelete.Text = "← ";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // textOperation
+            // 
+            this.textOperation.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textOperation.Location = new System.Drawing.Point(37, 68);
+            this.textOperation.Name = "textOperation";
+            this.textOperation.Size = new System.Drawing.Size(53, 13);
+            this.textOperation.TabIndex = 12;
+            // 
+            // textOperator1
+            // 
+            this.textOperator1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textOperator1.Location = new System.Drawing.Point(37, 49);
+            this.textOperator1.Name = "textOperator1";
+            this.textOperator1.Size = new System.Drawing.Size(53, 13);
+            this.textOperator1.TabIndex = 12;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxResult.Location = new System.Drawing.Point(35, 156);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(444, 72);
+            this.textBoxResult.TabIndex = 13;
+            this.textBoxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(504, 542);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textOperator1);
+            this.Controls.Add(this.textOperation);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonEquals);
+            this.Controls.Add(this.buttonNOT);
+            this.Controls.Add(this.buttonXOR);
+            this.Controls.Add(this.buttonOR);
+            this.Controls.Add(this.buttonAND);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button0);
+            this.Name = "Calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonAND;
+        private System.Windows.Forms.Button buttonOR;
+        private System.Windows.Forms.Button buttonXOR;
+        private System.Windows.Forms.Button buttonNOT;
+        private System.Windows.Forms.Button buttonEquals;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox textOperation;
+        private System.Windows.Forms.TextBox textOperator1;
+        private System.Windows.Forms.TextBox textBoxResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryCalculator
+{
+    enum Operation { 
+        AND,
+        OR,
+        XOR,
+        NOT,
+        NONE
+    }
+    public partial class Calculator : Form
+    {
+        private Operation currentOperation;
+        private String number;
+        private String operator1;
+        private String operator2;
+
+        public Calculator()
+        {
+            number = "";
+            operator1 = "";
+            operator2 = "";
+            currentOperation = Operation.NONE;
+            InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (number.Count() < 8) {
+                number += "0";
+                textBox.Text += "0";
+            }         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (number.Count() < 8)
+            {
+                number += "1";
+                textBox.Text += "1";
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "";
+            textOperator1.Text = "";
+            textOperation.Text = "";
+            number = "";
+            textBoxResult.Text = "";
+            currentOperation = Operation.NONE;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if(textBox.Text.Length > 0)
+            {
+                if(textBox.Text[textBox.Text.Length - 1] == ' ')
+                {
+                    while(textBox.Text[textBox.Text.Length - 1] != '0' && textBox.Text[textBox.Text.Length - 1] != '1')
+                    {
+                        textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1, 1);
+                    }
+                }
+                else
+                {
+                    textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1, 1);
+                    number = number.Remove(number.Length - 1, 1);
+                }               
+            }           
+        }
+
+        private void buttonAND_Click(object sender, EventArgs e)
+        {
+            currentOperation = Operation.AND;
+            if (textBoxResult.Text.Count() == 0)
+            {
+                textBox.Text += " AND ";
+                number = "";
+            }
+            else
+            {
+                textBox.Text = textBoxResult.Text;
+                textBox.Text += " AND ";
+                number = "";
+                textBoxResult.Text = "";
+            }           
+        }
+
+        private void buttonOR_Click(object sender, EventArgs e)
+        {
+            currentOperation = Operation.OR;
+            if (textBoxResult.Text.Count() == 0)
+            {
+                textBox.Text += " OR ";
+                number = "";
+                
+            }
+            else
+            {
+                textBox.Text = textBoxResult.Text;
+                textBox.Text += " OR ";
+                number = "";
+                textBoxResult.Text = "";
+            }            
+        }
+
+        private void buttonXOR_Click(object sender, EventArgs e)
+        {
+            currentOperation = Operation.XOR;
+            if (textBoxResult.Text.Count() == 0)
+            {
+                textBox.Text += " XOR ";
+                number = "";
+
+            }
+            else
+            {
+                textBox.Text = textBoxResult.Text;
+                textBox.Text += " XOR ";
+                number = "";
+                textBoxResult.Text = "";
+            }
+        }
+
+        private void buttonNOT_Click(object sender, EventArgs e)
+        {
+            currentOperation = Operation.NOT;
+            if (textBoxResult.Text.Count() == 0)
+            {
+                textBox.Text += " NOT ";
+                number = "";
+
+            }
+            else
+            {
+                textBox.Text = textBoxResult.Text;
+                textBox.Text += " NOT ";
+                number = "";
+                textBoxResult.Text = "";
+            }
+        }
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {
+            String num = "";
+            foreach(char c in textBox.Text) {
+                if (c == '1' || c == '0')
+                {
+                    num += c;
+                }else if(c == ' ' && num.Count() > 0)
+                {
+                    textOperator1.Text = num;
+                    operator1 = num;
+                    num = "";
+                }
+            }
+            operator2 = num;
+            textOperation.Text = num;
+            solveCurrentOperation();
+        }
+
+        private void solveCurrentOperation() {
+            setEqualLengths();
+            switch (currentOperation) {
+                case Operation.AND:
+                    solveAND();
+                    break;
+                case Operation.OR:
+                    solveOR();
+                    break;
+                case Operation.XOR:
+                    solveXOR();
+                    break;
+                case Operation.NOT:
+                    solveNOT();
+                    break;
+            }
+        }
+
+        private void setEqualLengths() {
+            String diff = "";
+
+            for(int i = 0; i < Math.Abs(operator1.Count() - operator2.Count()); i++)
+            {
+                diff += "0";
+            }
+
+            if(operator1.Count() < operator2.Count())
+            {
+                operator1 = operator1.Insert(0, diff);
+            }else if(operator2.Count() < operator1.Count())
+            {
+                operator2 = operator2.Insert(0, diff);
+            }
+        }
+
+        private void solveAND() {
+            for(int i = 0; i < operator1.Count(); i++)
+            {
+                if(operator1[i] == '1' && operator2[i] == '1')
+                {
+                    textBoxResult.Text += '1';
+                }
+                else
+                {
+                    textBoxResult.Text += '0';
+                }
+            }
+        }
+
+        private void solveOR()
+        {
+            for (int i = 0; i < operator1.Count(); i++)
+            {
+                if (operator1[i] == '1' || operator2[i] == '1')
+                {
+                    textBoxResult.Text += '1';
+                }
+                else
+                {
+                    textBoxResult.Text += '0';
+                }
+            }
+        }
+
+        private void solveXOR()
+        {
+            for (int i = 0; i < operator1.Count(); i++)
+            {
+                if ((operator1[i] == '1' || operator2[i] == '1') && operator1[i] != operator2[i])
+                {
+                    textBoxResult.Text += '1';
+                }
+                else
+                {
+                    textBoxResult.Text += '0';
+                }
+            }
+        }
+
+        private void solveNOT()
+        {
+            foreach(char bit in operator1)
+            {
+                if(bit == '1')
+                {
+                    textBoxResult.Text += '0';
+                }
+                else if(bit == '0')
+                {
+                    textBoxResult.Text += '1';
+                }
+            }
+        }        
+    }
+}
